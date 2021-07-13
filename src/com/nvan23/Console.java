27dfbadeac506aea0134e377804cf9eca946a069
Van Dam Thien Nhan <dtnvan@tma.com.vn>
2021-07-13 15:20:01 +0700

    implement OOP for mortgage calcultor

---
+++ src/com/nvan23/Console.java
@@ -0,0 +1,28 @@
+package com.nvan23;
+
+import java.util.Scanner;
+
+public class Console {
+  public static Scanner scanner = new Scanner(System.in);
+
+  public static double readNumber(String prompt) {
+    return scanner.nextDouble();
+  }
+  public static double readNUmber(
+          String prompt,
+          double min,
+          double max
+  ) {
+      Scanner scanner = new Scanner(System.in);
+      double values;
+      while (true) {
+          System.out.print(prompt);
+          values = scanner.nextDouble();
+          if (values >= min && values <= max)
+              break;
+
+          System.out.println("Enter a value between " + min + " and " + max + "\n");
+      }
+      return values;
+  }
+}
